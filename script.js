705cda96b90517c37ca4f65f07924ef085e249cb
errEstiak <estiak35-2998@diu.edu.bd>
2023-06-04 15:08:16 +0600

    sending to remote branch

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict'
+
+//STATEMENT
+//You work for a company buulding a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperatures amplitude. Keep in mind that sometimes there might be a sensor error."
+
+
+//given data
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+
+
+// SOLUTION
+//Ask questions
+//QUESTION 
+//what is temperature amplitude? : it's the diff. between mix & max temp.
+//how to find out min temp?
+//how to find out max temp?
+//what is sensor error
+
+//TODO
+//find out min temp
+//find out max temp
+//ignore the error
+//find out amplitude
+
+const calcTemperatureAmplitude = function(temps){    
+
+    let min = temps[0];
+    let max = temps[0];
+
+    for(let i=0; i < temps.length; i++){
+        let currTemp = temps[i];
+
+        if(typeof currTemp !== "number") continue;
+
+        if(currTemp < min){
+            min = currTemp;
+        }
+        if(currTemp > max){
+            max = currTemp;
+        }
+    }
+    const amplitude = max - min;
+    return amplitude;
+}
+
+console.log(temperatures);
+console.log(calcTemperatureAmplitude(temperatures));
+
+
