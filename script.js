e8b60a57502d0c16755432f907a0a204dbdd0f76
errEstiak <estiak35-2998@diu.edu.bd>
2023-06-04 15:19:45 +0600

    new problem statment & solution on new requirement branch

--- script.js
+++ script.js
@@ -49,3 +49,41 @@ console.log(temperatures);
 console.log(calcTemperatureAmplitude(temperatures));
 
 
+//PROBLEM 2
+// STATEMENT
+// instead of one array now the function will receive 2 arrays & all other functionalities will be same
+
+
+
+// SOLUTION
+// QUESTION
+// can a function receive 2 arrays together? : yes
+
+//TODO
+//merge two arrays
+
+const newCalcTemperatureAmplitude = function(t1, t2){
+
+    let temps = t1.concat(t2)
+    console.log(temps);
+
+    let min = temps[0];
+    let max = temps[0];
+
+    for(let i=0; i < temps.length; i++){
+        let currTemp = temps[i];
+
+        if(typeof currTemp !== "number") continue;
+
+        if(currTemp < min){
+            min = currTemp;
+        }
+        if(currTemp > max){
+            max = currTemp;
+        }
+    }
+    const amplitude = max - min;
+    return amplitude;
+}
+
+console.log(newCalcTemperatureAmplitude([2,3,4], [5,6,7]));
